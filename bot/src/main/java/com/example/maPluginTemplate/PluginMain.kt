3ac8644abd1a0269da3eed793295b00b7949c50d
洛-绫华 <75173095+qmzola@users.noreply.github.com>
2021-05-24 00:47:40 +0800

    Update PluginMain.kt

--- bot/src/main/java/com/example/maPluginTemplate/PluginMain.kt
+++ bot/src/main/java/com/example/maPluginTemplate/PluginMain.kt
@@ -9,7 +9,7 @@ object PluginMain : KotlinPlugin(
     JvmPluginDescription(
         id = "org.example.mirai-plugin",
         name = "ExamplePlugin",
-        version = "0.0.1"
+        version = "0.0.2"
     )
 ) {
     override fun onEnable() {
